933b48dffcee82d13f52e87eac1da754b136c73b
Marta <marta@ioc.cat>
2025-05-16 17:36:07 +0200

    Control d’errors per camps buits al formulari

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Agenda Web");
+
+document.querySelector("form").addEventListener("submit", e => {
+  e.preventDefault();
+  const input = document.querySelector("#nom");
+  if (input.value.trim() === "") {
+    alert("El camp no pot estar buit");
+    return;
+  }
+});
